26c0366a43ec5295086e7791f5fc60b4ed3b87aa
Volterra <volterra.luo@gmail.com>
2015-03-30 23:51:05 +0800

    init

---
+++ alipay/sign/MD5.py
@@ -0,0 +1,15 @@
+#! -*- coding: utf-8 -*-
+
+class MD5(object):
+
+	def  __init__(self, text='', key='', sign='',  input_charset='utf-8'):
+		this.text = text
+		this.key = key
+		this.sign = sign
+		this.input_charset = input_charset
+
+	def  sign(self):
+		pass
+
+	def verify(self):
+		pass
