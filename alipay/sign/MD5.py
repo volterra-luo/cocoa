f82a152dedc0773bd498f39d7014284ebc9a0af0
volterra-luo <volterra.luo@gmail.com>
2015-04-01 00:26:36 +0800

    init

--- alipay/sign/MD5.py
+++ alipay/sign/MD5.py
@@ -1,15 +1,15 @@
-#! -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import hashlib
 
 class MD5(object):
 
-	def  __init__(self, text='', key='', sign='',  input_charset='utf-8'):
+	def __init__(self, text='', key='', sign='', input_charset='utf-8'):
 		this.text = text
 		this.key = key
 		this.sign = sign
 		this.input_charset = input_charset
 
-	def  sign(self):
+	def sign(self):
 		m = hashlib.md5()
 		raw_str = this.text + this.key
 		tmp_str = unicode(raw_str, this.input_charset)
