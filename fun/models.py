19894a75f30413b2fcf32bf9394a1ee525ee058f
volterra-luo <volterra.luo@gmail.com>
2015-04-07 17:22:01 +0800

    init model in fun

--- fun/models.py
+++ fun/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import uuid
 
 class Tag(models.Model):
 	name = models.CharField(max_length='20', primary_key=True)
 
-class Joke(models.Models):
-	name = models.CharField(max_length='30', primary_key=True)
+class Joke(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	name = models.CharField(max_length='30')
 	content = models.TextField(blank=True)
 	tags = models.ManyToManyField(Tag)
